e2b19362f6ced2213d52e73da9d9121a51180d98
Norah-Tonny <norahtonny75@gmail.com>
2023-04-04 09:44:24 +0300

    created .env file

--- src/SendSms.js
+++ src/SendSms.js
@@ -1,16 +1,5 @@
-require('dotenv').config({path: __dirname + '/../.env'})
+ 
 
-const VONAGE_API_KEY = process.env.VONAGE_API_KEY
-const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET
-const TO_NUMBER = process.env.VONAGE_TO_NUMBER
-const VONAGE_BRAND_NAME = process.env.VONAGE_BRAND_NAME
-
-const { Vonage } = require('@vonage/server-sdk')
-
-const vonage = new Vonage({
-  apiKey: VONAGE_API_KEY,
-  apiSecret: VONAGE_API_SECRET
-})
 
 const from = VONAGE_BRAND_NAME
 const to = TO_NUMBER
@@ -27,28 +16,3 @@ sendSMS();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-
-// const SendSms=()=>{
-//   return (
-//     <SenContainer>
-      
-//     </SenContainer>
-//   )
-// }
-
-// export default SendSms
